c1afefc1a5620020e49ffe5bfd5d17527ce10e6b
Unknown <franciscomaria97@hotmail.com>
2018-03-29 23:32:49 +0100

    File is now passing by arguments ok

--- P1 - Distributed Backup Service/src/protocol/Backup.java
+++ P1 - Distributed Backup Service/src/protocol/Backup.java
@@ -1,18 +1,32 @@
 package protocol;
 
 import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
 
 public class Backup extends Thread{
 	
-	private File file;
+	private File file = null;
 	
 	public Backup(File file) {
 		this.file = file;
 	}
 	
 	public void run() {
-		
+	
 		System.out.println("STARTING BACKUP THREAD " + file.getAbsolutePath());
+		
+		Scanner input = null;
+		
+		try {
+			input = new Scanner(file);
+		} catch (FileNotFoundException e) {
+			System.out.println("It occurred an error with the file. File was not found.");
+		}
+		
+		while(input.hasNextLine()) {
+			System.out.println(input.nextLine());
+		}
 	}
 
 }
